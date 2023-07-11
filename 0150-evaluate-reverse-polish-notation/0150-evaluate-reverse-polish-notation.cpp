301762abbce704895b9a9938398e14bc0856764a
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-07-11 12:57:57 -0700

    Time: 11 ms (69.30%), Space: 12.1 MB (22.23%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,49 +2,44 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int>s;
-        for(int i=0; i<tokens.size(); i++) {
-            if(tokens[i]!="+" && tokens[i]!="/" && tokens[i]!="*" && tokens[i]!="-"){
+        for(auto it: tokens) {
+            if(it != "+" && it != "/" && it != "*" && it != "-"){
                 long long int num=0, chk=1;
-                for(int j=0; j<tokens[i].size(); j++){
-                    if(tokens[i][j]=='-'){chk=-1; continue;}
-                    num+=(tokens[i][j]-'0');
+                for(int j=0; j<it.size(); j++) {
+                    if(it[j]=='-'){chk=-1; continue;}
+                    num+=(it[j]-'0');
                     num*=10;
                 }
                 num/=10;
                 if(chk==-1){num*=(-1);}
-                //cout<<num<<endl;
                 s.push(num);
             }
-            else if(tokens[i]=="+"){
+            else if(it == "+"){
                 long long int t1=s.top();
                 s.pop();
                 long long int t2=s.top();
                 s.pop();
-                //cout<<t1<<" "<<t2<<endl;
                 s.push(t1+t2);
             }
-            else if(tokens[i]=="-"){
+            else if(it == "-"){
                 long long int t1=s.top();
                 s.pop();
                 long long int t2=s.top();
-                //cout<<t1<<" "<<t2<<endl;
                 s.pop();
                 s.push(t2-t1);
             }
-            else if(tokens[i]=="*"){
+            else if(it == "*"){
                 long long int t1=s.top();
                 s.pop();
                 long long int t2=s.top();
                 s.pop();
-                //cout<<t1<<" "<<t2<<endl;
                 s.push(t1*t2);
             }
-            else if(tokens[i]=="/"){
+            else if(it == "/"){
                 long long int t1=s.top();
                 s.pop();
                 long long int t2=s.top();
                 s.pop();
-                //cout<<t1<<" "<<t2<<endl;
                 s.push(t2/t1);
             }
         }
