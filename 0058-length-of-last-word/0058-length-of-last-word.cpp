5ab208bd5bfd8273afe4ac86f6a6435fcf05a593
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-07-11 12:32:16 -0700

    Time: 5 ms (39.27%), Space: 6.4 MB (86.78%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    string trim(string s) {
-        int x,y;
-        for(int i=0;i<s.size();i++) {
-            if(s[i]!=' '){ 
-                x=i;
-                break;
-            }
-        }
-        for(int i=s.size()-1;i>=0;i--) {
-            if(s[i]!=' '){ 
-                y=i;
-                break;
-            }
-        }
-        s=s.substr(x,y-x+1);
-        return s;
-    }
     int lengthOfLastWord(string s) {
-        s = trim(s);
-        int n = s.size();
-        string ans = "";
-        int f = 0;
-        for(int i=n-1;i>=0;i--) {
-            if(s[i]==' ') {
-                f = 1;
-                ans = s.substr(i+1);
-                break;
+        int siz=s.size(),kount=0,flag=0;
+        for(int i=siz-1;i>=0;i--){
+            if(s[i] == ' ' && flag) break;
+            if(s[i] != ' ') {
+                flag=1;
+                kount++;
             }
         }
-        if(f==0) return n;
-        return ans.size();
+        return kount;
     }
 };
