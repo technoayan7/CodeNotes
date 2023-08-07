0a484cf30b8226777955c2a1b258c0c14c3eec8d
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-08-07 12:05:22 -0700

    Time: 2 ms (44.24%), Space: 6.2 MB (10.89%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+    
+        while(b != 0){
+            unsigned int tmp = (a & b);
+            a = a ^ b;
+            b = tmp << 1;
+        }
+        return a;
+    }
+};
