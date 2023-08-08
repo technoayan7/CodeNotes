208870925c0c227d5fb1cfac576e98658ab02262
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-08-08 10:25:55 -0700

    Time: 21 ms (76.57%), Space: 28.8 MB (63.01%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool dfs(TreeNode*root,TreeNode*target) {
+        if(root==NULL && target==NULL) return true;
+        if(root==NULL|| target==NULL) return false;
+        if(root->val != target->val) return false;
+        return dfs(root->left,target->left) && dfs(root->right,target->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root) return false;
+        
+        if(dfs(root, subRoot)) return true;
+        
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
