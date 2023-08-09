7d69c2eaedce96e99a9be268fc033dcdc98924a6
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-08-09 13:10:26 -0700

    Time: 5 ms (66.53%), Space: 13.8 MB (40.69%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int rob1(vector<int>& cost, vector<int>& dp1, int i) {
+    int rob(vector<int>& cost, vector<int>& dp, int i) {
         if(i >= cost.size()) return 0;
-        if(dp1[i] != -1) return dp1[i];
-        int notTake = cost[i] + rob1(cost, dp1, i+1);
-        int take = cost[i] + rob1(cost, dp1, i+2);
-        return dp1[i] = min(notTake, take);
-    }
-    int rob2(vector<int>& cost, vector<int>& dp2, int i) {
-        if(i >= cost.size()) return 0;
-        if(dp2[i] != -1) return dp2[i];
-        int notTake = cost[i] + rob1(cost, dp2, i+1);
-        int take = cost[i] + rob1(cost, dp2, i+2);
-        return dp2[i] = min(notTake, take);
+        if(dp[i] != -1) return dp[i];
+        int notTake = cost[i] + rob(cost, dp, i+1);
+        int take = cost[i] + rob(cost, dp, i+2);
+        return dp[i] = min(notTake, take);
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp1(n, -1);
-        vector<int> dp2(n, -1);
-        int ans1 = rob1(cost, dp1, 0);
-        int ans2 = rob2(cost, dp2, 1);
+        vector<int> dp(n, -1);
+        int ans1 = rob(cost, dp, 0);
+        int ans2 = rob(cost, dp, 1);
         if(n == 1) return cost[0];
         return min(ans1, ans2);
     }
