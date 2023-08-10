7453be7fbd051e37827575dafd980314d968d97f
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-08-10 00:30:07 -0700

    Time: 12 ms (28.04%), Space: 10.4 MB (22.70%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+	bool f(int ind, int target, vector<int> & arr, vector<vector<int>> & dp) {
+        if (target == 0)
+            return dp[ind][target]=true;
+
+        if (ind == 0)
+            return dp[ind][target] = arr[0] == target;
+
+        if (dp[ind][target] != -1)
+            return dp[ind][target];
+
+        bool notTaken = f(ind - 1, target, arr, dp);
+
+        bool taken = false;
+        if (arr[ind] <= target)
+            taken = f(ind - 1, target - arr[ind], arr, dp);
+
+        return dp[ind][target] = notTaken || taken;
+    }
+
+	int lastStoneWeightII(vector<int>& arr) {
+		int totSum = 0, n = arr.size();
+        for (int i = 0; i < n; i++) {
+            totSum += arr[i];
+        }
+        vector < vector < int >> dp(n, vector < int > (totSum + 1, -1));
+        for (int i = 0; i <= totSum; i++) {
+            bool dummy = f(n - 1, i, arr, dp);
+        }
+
+        int mini = 1e9;
+        for (int i = 0; i <= totSum; i++) {
+            if (dp[n - 1][i] == true) {
+                int diff = abs(i - (totSum - i));
+                mini = min(mini, diff);
+            }
+        }
+        return mini;
+	}
+};
