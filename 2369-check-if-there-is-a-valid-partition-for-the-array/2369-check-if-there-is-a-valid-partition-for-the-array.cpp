bf55615680dfbfef1c313d9bf5c1e86c59458c86
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2023-08-13 12:37:52 -0700

    Time: 127 ms (88.99%), Space: 93 MB (15.90%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,21 +1,21 @@
 class Solution {
     public:
-       	bool f(int i, vector<int> &nums, vector<int> &dp) {
-            if (i < 0) return 1;
+       	bool f(int i, vector<int> &nums, vector<int> &dp, int n) {
+            if (i >= n) return 1;
        	    if (dp[i] != -1) return dp[i];
        	    bool two = 0;
        	    bool three = 0;
        	    bool seq = 0;
                 
                 
-       	    if (i >= 1 && nums[i] == nums[i - 1])
-       	        two = f(i - 2, nums, dp);
+       	    if (i+1 < n && nums[i] == nums[i + 1])
+       	        two = f(i + 2, nums, dp, n);
                 
-       	    if (i >= 2 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2])
-       	        three = f(i - 3, nums, dp);
+       	    if (i+2 < n && nums[i] == nums[i + 1] && nums[i + 1] == nums[i + 2])
+       	        three = f(i + 3, nums, dp, n);
 
-       	    if (i >= 2 && nums[i] == nums[i - 1] + 1 && nums[i - 1] == nums[i - 2] + 1)
-       	        seq = f(i - 3, nums, dp);
+       	    if (i+2 < n && nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2])
+       	        seq = f(i + 3, nums, dp, n);
             
        	    return dp[i] = two || three || seq;
        	
@@ -23,6 +23,6 @@ class Solution {
        	bool validPartition(vector<int> &nums) {
        	    int n = nums.size();
        	    vector<int> dp(n, -1);
-       	    return f(n - 1, nums, dp);
+       	    return f(0, nums, dp, n);
        	}
 };
