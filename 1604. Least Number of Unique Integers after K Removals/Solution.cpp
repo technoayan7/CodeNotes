db5271403a2b33b22c4868813efe2859ed071f4d
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-02-16 10:15:45 -0800

    C++ Solution
            Submission Statistics:
            Question #: 1604
            Language: C++
            Runtime: 177 ms
            Runtime percentile: 43.92
            Memory: 80.956 MB
            Memory percentile: 21.09

--- 1604. Least Number of Unique Integers after K Removals/Solution.cpp
+++ 1604. Least Number of Unique Integers after K Removals/Solution.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     static bool cmp(pair<int, int> a, pair<int, int> b) {
-        if (a.second == b.second)
-            return a.first > b.first;
         return a.second < b.second;
     }
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
