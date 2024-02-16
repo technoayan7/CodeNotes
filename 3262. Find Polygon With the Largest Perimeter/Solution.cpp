2760ff7752af04762de4188f80ea582cec2c2ce4
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-02-16 08:33:59 -0800

    C++ Solution
            Submission Statistics:
            Question #: 3262
            Language: C++
            Runtime: 135 ms
            Runtime percentile: 87.11
            Memory: 83.316 MB
            Memory percentile: 35.31

--- 3262. Find Polygon With the Largest Perimeter/Solution.cpp
+++ 3262. Find Polygon With the Largest Perimeter/Solution.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     long long largestPerimeter(vector<int>& nums) {
         int n = nums.size();
-        int sides = n;
         sort(nums.begin(), nums.end());
         long long p = 0;
         for(auto it: nums)  p += it;
         for(int i=n-1;i>=2;i--) {
-            if(p <= 2*nums[i]) {
-                p -= nums[i];
-                sides--;
+            p -= nums[i];
+            if(p > nums[i]) {
+                return p + nums[i];
             }
-            else break;
         }
-        if(sides == 2) return -1;
-        return p;
+        return -1;
     }
 };
