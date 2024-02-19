851afe903c8438190b50efdbc55394ed42b487d9
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-02-18 23:24:25 -0800

    C++ Solution
            Submission Statistics:
            Question #: 231
            Language: C++
            Runtime: 5 ms
            Runtime percentile: 20.09
            Memory: 7.1 MB
            Memory percentile: 49.03

--- 231. Power of Two/Solution.cpp
+++ 231. Power of Two/Solution.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        int l = 0, h = n;
-        while(l<=h) {
-            int mid = (l+h)/2;
-            if(pow(2,mid) == abs(n)) return true;
-            else if(pow(2,mid) < n) l = mid+1;
-            else h = mid-1;
+        int ans = 1;
+        for(int i=0; i<30; i++){
+            if(ans == n) return true;
+            ans = ans * 2;
+            if(ans == n) return true;
         }
         return false;
     }
