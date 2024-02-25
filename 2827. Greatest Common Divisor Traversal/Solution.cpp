e44d86475b714410b5cad322a90ebcb7b90bad38
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-02-24 23:47:34 -0800

    C++ Solution
            Submission Statistics:
            Question #: 2827
            Language: C++
            Runtime: 755 ms
            Runtime percentile: 49.10
            Memory: 124.34 MB
            Memory percentile: 58.08

--- 2827. Greatest Common Divisor Traversal/Solution.cpp
+++ 2827. Greatest Common Divisor Traversal/Solution.cpp
@@ -1,74 +1,75 @@
-class Solution {
-    // Function to find the root of a set using path compression
-    int getf(vector<int> &f, int x) {
-        return f[x] == x ? x : (f[x] = getf(f, f[x]));
-    }
-    
-    // Function to merge two sets and update the frequency
-    void merge(vector<int> &f, vector<int> &num, int x, int y) {
-        x = getf(f, x);
-        y = getf(f, y);
-        if (x == y) {
-            return;
-        }
-        if (num[x] < num[y]) {
-            swap(x, y);
-        }
-        f[y] = x;
-        num[x] += num[y];
-    }
+class DisjointSet 
+{
 public:
-    // Main function to check if it is possible to traverse all pairs
-    bool canTraverseAllPairs(vector<int>& nums) {
-        const int n = nums.size();
-        if (n == 1) {
-            return true;
+    vector<int> parent, size;
+    DisjointSet(int n) 
+    {
+        parent.resize(n);
+        size.resize(n);
+        for(int x=0; x<n; x++) 
+        {
+            parent[x]=x;
+            size[x]=1;
         }
-        
-        // Initialize Union-Find data structures
-        vector<int> f(n), num(n);
-        for (int i = 0; i < n; ++i) {
-            f[i] = i;
-            num[i] = 1;
+    }
+
+    int findPar(int node) 
+    {
+        if(node==parent[node]) return node;
+        return parent[node]=findPar(parent[node]);
+    }
+
+    void unionn(int u, int v) 
+    {
+        int upar=findPar(u);
+        int vpar=findPar(v);
+        if(upar==vpar) return;
+        if(size[upar]<size[vpar]) 
+        {
+            parent[upar]=vpar;
+            size[vpar]+=size[upar];
         }
-        
-        // Map to store the first index encountered for each prime factor
-        unordered_map<int, int> have;
-        
-        // Iterate through the array to find common prime factors and perform Union-Find
-        for (int i = 0; i < n; ++i) {
-            int x = nums[i];
-            if (x == 1) {
-                return false;
-            }
-            
-            // Iterate through prime factors of x
-            for (int d = 2; d * d <= x; ++d) {
-                if (x % d == 0) {
-                    // If the prime factor is encountered before, merge the sets
-                    if (have.count(d)) {
-                        merge(f, num, i, have[d]);
-                    } else {
-                        // Otherwise, store the index for this prime factor
-                        have[d] = i;
-                    }
-                    while (x % d == 0) {
-                        x /= d;
-                    } 
-                }
-            }
-            
-            // Handle the case where x itself is a prime factor
-            if (x > 1) {
-                if (have.count(x)) {
-                    merge(f, num, i, have[x]);
-                } else {
-                    have[x] = i;
-                }
+        else 
+        {
+            parent[vpar]=upar;
+            size[upar]+=size[vpar];
+        }
+    }
+};
+class Solution 
+{
+public:
+    map<int,vector<int>> m;
+    void primeFactors(int n, int i) 
+    {
+        if(n%2==0) m[2].push_back(i);
+        while(n%2==0) n/=2;
+        for(int x=3; x<=sqrt(n); x+=2) 
+        { 
+            if(n%x==0) m[x].push_back(i);
+            while(n%x==0) n/=x; 
+        }
+        if(n>2) m[n].push_back(i);
+    } 
+
+    bool canTraverseAllPairs(vector<int>& nums) 
+    {
+        int n=nums.size();
+        DisjointSet ds(n);
+        for(int x=0; x<n; x++) primeFactors(nums[x],x);
+        for(auto it: m)
+        {
+            auto indices=it.second;
+            for(int x=1; x<indices.size(); x++)
+            {
+                ds.unionn(indices[0],indices[x]);
             }
         }
-        
-        // Check if all indices belong to the same connected component
-        return num[getf(f, 0)] == n;
+        set<int> parents;
+        for(int x=0; x<n; x++)
+        {
+            parents.insert(ds.findPar(x));
+        }
+        return parents.size()==1;
     }
 };
