e809a3da7f4a9605534e5cbff41f48e7fd9cb8d6
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-03-01 00:46:27 -0800

    C++ Solution
            Submission Statistics:
            Question #: 3055
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 9.136 MB
            Memory percentile: 16.21

--- 3055. Maximum Odd Binary Number/Solution.cpp
+++ 3055. Maximum Odd Binary Number/Solution.cpp
@@ -6,14 +6,7 @@ public:
             if(it=='1') c++;
             else d++;
         }
-        string ans="";
-        for(int i=0;i<c-1;i++) {
-            ans += '1';
-        }
-        for(int i=0;i<d;i++) {
-            ans += '0';
-        }
-        ans += '1';
+        string ans = string(c - 1, '1') + string(d, '0') + '1';
         return ans;
     }
 };
