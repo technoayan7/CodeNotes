b7a61f944566176d4bf9aedc4415e49a34a3ef31
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-12-02 23:49:38 -0800

    C++ Solution
            Submission Statistics:
            Question #: 2232
            Language: C++
            Runtime: 41 ms
            Runtime percentile: 6.02
            Memory: 85.676 MB
            Memory percentile: 16.63

---
+++ 2232. Adding Spaces to a String/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        sort(spaces.begin(),spaces.end());
+        int l=spaces.size(),n=s.length();
+        int j=0;
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(j<l){
+                if(i==spaces[j]){
+                    ans+=' ';
+                    j++;
+                }
+            }
+                ans+=s[i];
+        }
+        return ans;
+    }
+};
