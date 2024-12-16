91be93b18f3d045e0a463ae255f611fd8d0b4de7
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-12-15 17:38:09 -0800

    C++ Solution
            Submission Statistics:
            Question #: 3555
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 27.204 MB
            Memory percentile: 38.94

--- 3555. Final Array State After K Multiplication Operations I/Solution.cpp
+++ 3555. Final Array State After K Multiplication Operations I/Solution.cpp
@@ -1,21 +1,9 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> getFinalState(vector<int> &arr, int k, int mult)
-    {
-        int n = arr.size();
-        for (int i = 0; i < k; ++i)
-        {
-            int ind = 0;
-            for (int j = 1; j < n; ++j)
-            {
-                if (arr[j] < arr[ind])
-                {
-                    ind = j;
-                }
-            }
-            arr[ind] *= mult;
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        while (k--) {
+            *min_element(begin(nums), end(nums)) *= multiplier;
         }
-        return arr;
+        return nums;
     }
 };
