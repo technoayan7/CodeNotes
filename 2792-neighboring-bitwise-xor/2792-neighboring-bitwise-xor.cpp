ca4de0cf3e655c124c54caa209b15050a8840adc
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-01-18 01:22:27 +0530

    Time: 0 ms (100%), Space: 264 MB (50.63%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& d) {
-        int x=0; 
-        for(int c:d) x^=c;
+        int x = 0;
+        for (int c : d) {
+            x ^= c;
+        }
         return (!x);
     }
 };
