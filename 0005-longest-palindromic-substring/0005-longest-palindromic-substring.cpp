1493f7c3fbca09a9ca7fbea7f32cfa98af1585dc
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-06-22 21:52:26 +0530

    Time: 5 ms (91.73%), Space: 9.4 MB (69.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
-        int n = s.length();
-        int low,high;
-        
-        int st=0, len=1; 
-        
-        for(int i=1;i<n;i++){
-            
-            //even case
+        int n = s.size();
+        int low, high;
+        int st=0;
+        int len = 0;
+        for (int i = 0; i < n; i++) {
             low = i;
-            high = i-1;
-            while(low>=0 && high<n && s[low]==s[high]){
-                
-                if(high-low+1 > len){
-                    st = low;
-                    len = high-low+1;
-                }
-                low--;high++;
-            }
-            
-            //odd case
-            low = i-1;
             high = i+1;
-            while(low>=0 && high<n && s[low]==s[high]){
-                
+            while (low >= 0 and high < n and s[low] == s[high]) {
                 if(high-low+1 > len){
                     st = low;
                     len = high-low+1;
@@ -33,52 +16,17 @@ public:
                 low--;
                 high++;
             }
-            
-            
+            low = i;
+            high = i;
+            while (low >= 0 and high < n and s[low] == s[high]) {
+                if(high-low+1 > len){
+                    st = low;
+                    len = high-low+1;
+                }
+                low--;
+                high++;
+            }
         }
-        
-        return s.substr(st,len);
-       
+        return s.substr(st, len);
     }
 };
-
-
-// class Solution {
-// private: 
-//     bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-//         if(i == j){
-//             return dp[i][j] = true;
-//         }
-//         if(j-i == 1){
-//             if(s[i] == s[j]){
-//                 return dp[i][j] = true;
-//             }
-//             else{
-//                 return dp[i][j] = false;
-//             }
-//         }
-//         if(s[i] == s[j] && dp[i+1][j-1] == true){
-//             return dp[i][j] = true;
-//         } else {
-//             return dp[i][j] = false;
-//         }
-//     }
-// public:
-//     string longestPalindrome(string s) {
-//         int n = s.size();
-//         int startIndex = 0; int maxlen = 0;
-//         vector<vector<bool>> dp(n, vector<bool>(n, false));
-//         for(int g=0; g<n; g++){
-//             for(int i=0, j=g; j<n; i++, j++){
-//                 solve(dp, i, j, s);
-//                 if(dp[i][j] == true){
-//                     if(j-i+1 > maxlen){
-//                         startIndex = i;
-//                         maxlen = j-i+1;
-//                     }
-//                 }
-//             }
-//         }
-//         return s.substr(startIndex, maxlen);
-//     }
-// };
