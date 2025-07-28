364c6f8f4b656ad10734136d460d0da095eca10c
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-07-28 23:24:55 +0530

    Time: 22 ms (46.14%), Space: 10.6 MB (86.47%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void findSubsets(int ind, vector<int> &nums, vector<int> &ds) {
+    int cnt = 0;
+    void findSubsets(int ind, vector<int> &nums, vector<int> &ds, int &maxOR) {
         if (ind == nums.size()) {
-            ans.push_back(ds);
+            int tot = 0;
+            for(auto it1: ds) {
+                tot |= it1;
+            }
+            if(tot == maxOR) cnt++;
             return;
         }
          
         ds.push_back(nums[ind]);
-        findSubsets(ind + 1, nums, ds);
+        findSubsets(ind + 1, nums, ds, maxOR);
         ds.pop_back();
 
-        findSubsets(ind + 1, nums, ds);
+        findSubsets(ind + 1, nums, ds, maxOR);
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int n = nums.size();
-
         int maxOR = 0;
         for(int i=0;i<n;i++){
             maxOR = maxOR | nums[i];
         }
         vector<int> ds;
         sort(nums.begin(), nums.end());
-        findSubsets(0, nums, ds);
-        int cnt = 0;
-        for(auto it: ans) {
-            int tot = 0;
-            for(auto it1: it) {
-                tot |= it1;
-            }
-            if(tot == maxOR) cnt++;
-        }
+        findSubsets(0, nums, ds, maxOR);
         return cnt;
     }
 };
