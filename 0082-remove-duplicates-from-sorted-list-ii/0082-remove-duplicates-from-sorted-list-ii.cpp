2306b0c0ba9750188ba1a3cfaf2d65da7a128a4c
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2025-08-09 03:29:14 +0530

    Time: 0 ms (100%), Space: 15.6 MB (72.38%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,11 +10,14 @@ public:
                 flag = 1;
                 curr = curr->next;
             }
-            if(flag == 0) {
-                prev->next = curr;
-                prev = prev->next;
+            if(flag == 1) {
+                curr = curr->next;
             }
-            curr = curr->next;
+            else {
+                prev->next = new ListNode(curr->val);
+                prev = prev->next;
+                curr = curr->next;
+            }
         }
         return newHead->next;
     }
